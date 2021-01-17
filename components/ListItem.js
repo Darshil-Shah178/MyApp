fc2f0a8cff57e447421cd3b1d5abeb8b666045c6
DARSHIL SHAH <my.name@example.com>
2021-01-17 08:45:39 +0200

    Codes for Task B

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
-import PropTypes from 'prop-types';
+import React from "react";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+
+const uploadsUrl = "http://media.mw.metropolia.fi/wbma/uploads/";
 
 const ListItem = (props) => {
   return (
     <TouchableOpacity style={styles.row}>
-      <View style={styles.imagebox}>
-        <Image
-          style={styles.image}
-          source={{uri: props.singleMedia.thumbnails.w160}}
-        />
-      </View>
+      <Image
+        style={styles.image}
+        source={{ uri: uploadsUrl + props.singleMedia.thumbnails.w160 }}
+      />
       <View style={styles.textbox}>
         <Text style={styles.listTitle}>{props.singleMedia.title}</Text>
         <Text>{props.singleMedia.description}</Text>
@@ -21,27 +21,24 @@ const ListItem = (props) => {
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 15,
     marginBottom: 5,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
     borderRadius: 6,
   },
-  listTitle: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    paddingBottom: 15,
+  textbox: {
+    flex: 1,
+    padding: 10,
   },
   image: {
     flex: 1,
     borderRadius: 6,
   },
-  textbox: {
-    flex: 2,
-    padding: 10,
-  },
-  imagebox: {
-    flex: 1,
+  listTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingBottom: 15,
   },
 });
 
